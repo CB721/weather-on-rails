f6d62238176bb96c81e17110ebf816c608ca458c
Clint Brodar <clintbrodar@gmail.com>
2020-06-17 13:14:49 -0700

    API key moved to .env file

--- app/controllers/api/v1/weather_controller.rb
+++ app/controllers/api/v1/weather_controller.rb
@@ -1,12 +1,15 @@
 require 'net/http'
+require 'dotenv'
 
 module Api
     module V1
         class WeatherController < ApplicationController
             def index
+                Dotenv.load
+                key = ENV['WEATHER_KEY']
                 lat = params[:lat]
                 long = params[:long]
-                uri = URI("http://api.openweathermap.org/data/2.5/weather?lat=#{lat}&lon=#{long}&appid=95e1597bd5768bde56e965c059d28cdf&units=imperial")
+                uri = URI("http://api.openweathermap.org/data/2.5/weather?lat=#{lat}&lon=#{long}&appid=#{key}&units=imperial")
                 weather = Net::HTTP.get(uri);
                 render json: {status: 'SUCCESS', message: 'Loaded Weather', data: weather}, status: :ok
             end
