c22c37ddc9f9e4f81ea8854a17cbe0fe53fe1294
Clint Brodar <clintbrodar@gmail.com>
2020-06-18 14:53:41 -0700

    Removed forecast route

--- app/controllers/api/v1/weather_controller.rb
+++ app/controllers/api/v1/weather_controller.rb
@@ -14,16 +14,6 @@ module Api
                 render json: {status: 'SUCCESS', message: 'Loaded Weather', data: weather}, status: 201
             end
 
-            def forecast
-                Dotenv.load
-                key = ENV['WEATHER_KEY']
-                lat = params[:lat]
-                long = params[:long]
-                forecastWeather = URI("https://api.openweathermap.org/data/2.5/forecast?q=?lat=#{lat}&lon=#{long}&appid=#{key}&units=imperial&mode=json")
-                forecast = Net::HTTP.get(forecastWeather)
-                render json: {status: 'SUCCESS', message: 'Loaded Weather', data: forecast}, status: 201
-            end
-
             def delete
                 render json: {status: 'Error', message: 'Invalid Request'}, status: :unprocessable_entity
             end
