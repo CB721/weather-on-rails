afab1f704f88a39678a0aba82a62763805e63bbb
Clint Brodar <clintbrodar@gmail.com>
2020-09-13 12:09:26 -0700

    set CORS to all

--- app/controllers/api/v1/weather_controller.rb
+++ app/controllers/api/v1/weather_controller.rb
@@ -5,7 +5,7 @@ module Api
     module V1
         class WeatherController < ApplicationController
             def index
-                headers['Access-Control-Allow-Origin'] = 'https://weather-app-cb.herokuapp.com/'
+                headers['Access-Control-Allow-Origin'] = '*'
                 headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
                 headers['Access-Control-Request-Method'] = '*'
                 headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
@@ -19,7 +19,7 @@ module Api
             end
 
             def create
-                headers['Access-Control-Allow-Origin'] = 'https://weather-app-cb.herokuapp.com/'
+                headers['Access-Control-Allow-Origin'] = '*'
                 headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
                 headers['Access-Control-Request-Method'] = '*'
                 headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
