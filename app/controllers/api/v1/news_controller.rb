653726a0d801b9399f0f83b448e137ec3ed802c6
Clint Brodar <clintbrodar@gmail.com>
2020-09-13 12:05:23 -0700

    update cors to deployed client app

--- app/controllers/api/v1/news_controller.rb
+++ app/controllers/api/v1/news_controller.rb
@@ -5,7 +5,7 @@ module Api
     module V1
         class NewsController < ApplicationController
             def index
-                headers['Access-Control-Allow-Origin'] = '*'
+                headers['Access-Control-Allow-Origin'] = 'https://weather-app-cb.herokuapp.com/'
                 headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
                 headers['Access-Control-Request-Method'] = '*'
                 headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
